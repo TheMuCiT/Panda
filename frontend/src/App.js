8d57341922fb43c72edb4057effc49df3dc12cdb
THeMuCiT127 <themucit127@gmail.com>
2021-09-28 14:26:20 +0300

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+//, { useState }
 import Navbar from './components/navbar/Navbar'
 import Main from './pages/Main'
 import Footer from './components/footer/Footer'
 
-import bc from './images/BC.png'
+//import bc from './images/BC.png'
 
 const App = () => {
-     const [press, setpress] = useState(true)
+     /*const [press, setpress] = useState(true)
      const handlerClick = () => {
           if (press === false) {
                setpress(true)
           } else {
                setpress(false)
           }
-     }
+     }*/
      return (
           <>
-               <img src={bc} className={press ? 'BC' : 'BCNONE'} alt='Error' onClick={handlerClick} />
+               {/* <img src={bc} className={press ? 'BC' : 'BCNONE'} alt='Error' onClick={handlerClick} /> */}
                <Navbar />
                <Main />
                <Footer />
