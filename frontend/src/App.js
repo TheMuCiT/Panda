954ef28495a14afcbcaa2d3eda097c0e99194a7b
THeMuCiT127 <themucit127@gmail.com>
2021-10-05 02:18:06 +0300

    mobile fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react'
-//, { useState }
+import React, { useEffect } from 'react'
 import Navbar from './components/navbar/Navbar'
 import Main from './pages/Main'
 import Footer from './components/footer/Footer'
@@ -7,20 +6,26 @@ import Footer from './components/footer/Footer'
 import { ReactComponent as BC1 } from './icons/BC1.svg'
 import { ReactComponent as BC2 } from './icons/BC2.svg'
 
-//import bc from './images/BC.png'
-
 const App = () => {
-     /*const [press, setpress] = useState(true)
-     const handlerClick = () => {
-          if (press === false) {
-               setpress(true)
+     let bc1 = document.getElementsByClassName('BC1')
+     let bc2 = document.getElementsByClassName('BC2')
+     useEffect(() => {
+          ShowMobile()
+     })
+
+     const ShowMobile = () => {
+          if (window.innerWidth < 900) {
+               bc1[0].style.opacity = 0
+               bc2[0].style.opacity = 0
           } else {
-               setpress(false)
+               bc1[0].style.opacity = 1
+               bc2[0].style.opacity = 1
           }
-     }*/
+     }
+
+     window.addEventListener('resize', ShowMobile)
      return (
           <>
-               {/* <img src={bc} className={press ? 'BC' : 'BCNONE'} alt='Error' onClick={handlerClick} /> */}
                <BC1 className='BC1' />
                <BC2 className='BC2' />
                <Navbar />
--- frontend/src/components/Animation/Animation.js
+++ frontend/src/components/Animation/Animation.js
@@ -10,6 +10,7 @@ const Animation = () => {
      window.onscroll = function () {
           scrollRotate()
      }
+
      const scrollRotate = () => {
           let offsetMerhaba = 300
           let MerhabaDuration = 250
@@ -41,24 +42,41 @@ const Animation = () => {
           let WindowsPositionhukuk = window.pageYOffset
           let Movehukuk = SectionStarthukuk - WindowsPositionhukuk
           Movehukuk = HukukDuration - Movehukuk
-          image2[0].style.transform = `translateX(${-225}px)`
-          image2[1].style.transform = `translateX(${80}px)`
+          let firstHukuk = -225
+          let secondHukuk = 80
+          let index = 1
+          let index1 = 1
+          if (window.innerWidth < 1350) {
+               index = 1.875
+               index1 = 1.25
+               firstHukuk = -120
+               secondHukuk = 64
+          }
+          if (window.innerWidth < 900) {
+               index = 3
+               index1 = 2
+               firstHukuk = -75
+               secondHukuk = 40
+          }
+
+          image2[0].style.transform = `translateX(${firstHukuk}px)`
+          image2[1].style.transform = `translateX(${secondHukuk}px)`
 
           if (
                SectionStarthukuk - HukukDuration - 150 <= WindowsPositionhukuk &&
                SectionStarthukuk - 150 >= WindowsPositionhukuk
           ) {
                Movehukuk += 150
-               image2[0].style.transform = `translateX(${-225 + Movehukuk / 2}px)`
-               image2[1].style.transform = `translateX(${80 - Movehukuk / 5.625}px)`
+               image2[0].style.transform = `translateX(${firstHukuk + Movehukuk / 2 / index}px)`
+               image2[1].style.transform = `translateX(${secondHukuk - Movehukuk / 5.625 / index1}px)`
           }
           if (
                SectionStarthukuk + HukukDuration + 150 >= WindowsPositionhukuk &&
                SectionStarthukuk + 150 <= WindowsPositionhukuk
           ) {
                Movehukuk = Movehukuk - HukukDuration - 150
-               image2[0].style.transform = `translateX(${0 - Movehukuk / 2}px)`
-               image2[1].style.transform = `translateX(${0 + Movehukuk / 5.625}px)`
+               image2[0].style.transform = `translateX(${0 - Movehukuk / 2 / index}px)`
+               image2[1].style.transform = `translateX(${0 + Movehukuk / 5.625 / index1}px)`
           }
           if (SectionStarthukuk - 150 <= WindowsPositionhukuk && SectionStarthukuk + 150 > WindowsPositionhukuk) {
                image2[0].style.transform = `translateX(${0}px)`
@@ -74,30 +92,49 @@ const Animation = () => {
           let WindowsPositionogren = window.pageYOffset
           let Movehuogren = SectionStartogren - WindowsPositionogren
           Movehuogren = OgrenDuration - Movehuogren
-          image3[0].style.transform = `translateX(${185}px)`
-          image3[1].style.transform = `translateX(${-165}px)`
+          let firstOgren = 185
+          let secondOgren = -165
+          let index2 = 1
+          let index3 = 1
+          if (window.innerWidth < 1350) {
+               index2 = 1.25
+               index3 = 1.25
+               firstOgren = 148
+               secondOgren = -132
+          }
+
+          if (window.innerWidth < 900) {
+               index2 = 2.3125
+               index3 = 2
+               firstOgren = 80
+               secondOgren = -82
+          }
+          image3[0].style.transform = `translateX(${firstOgren}px)`
+          image3[1].style.transform = `translateX(${secondOgren}px)`
 
           if (
                SectionStartogren - OgrenDuration - 150 <= WindowsPositionogren &&
                SectionStartogren - 150 >= WindowsPositionogren
           ) {
                Movehuogren += 150
-               image3[0].style.transform = `translateX(${185 - Movehuogren / 2.4}px)`
-               image3[1].style.transform = `translateX(${-165 + Movehuogren / 2.72}px)`
+               image3[0].style.transform = `translateX(${firstOgren - Movehuogren / 2.4 / index2}px)`
+               image3[1].style.transform = `translateX(${secondOgren + Movehuogren / 2.72 / index3}px)`
           }
           if (
                SectionStartogren + OgrenDuration + 150 >= WindowsPositionogren &&
                SectionStartogren + 150 <= WindowsPositionogren
           ) {
                Movehuogren = Movehuogren - OgrenDuration - 150
-               image3[0].style.transform = `translateX(${0 + Movehuogren / 2.4}px)`
-               image3[1].style.transform = `translateX(${0 - Movehuogren / 2.72}px)`
+               image3[0].style.transform = `translateX(${0 + Movehuogren / 2.4 / index2}px)`
+               image3[1].style.transform = `translateX(${0 - Movehuogren / 2.72 / index3}px)`
           }
           if (SectionStartogren - 150 <= WindowsPositionogren && SectionStartogren + 150 > WindowsPositionogren) {
                image3[0].style.transform = `translateX(${0}px)`
                image3[1].style.transform = `translateX(${0}px)`
           }
      }
+
+     window.addEventListener('resize', scrollRotate)
 
      return <></>
 }
