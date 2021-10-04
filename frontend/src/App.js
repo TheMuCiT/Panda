f6602681655717088408a944a53ffb480624a1a2
THeMuCiT127 <themucit127@gmail.com>
2021-10-04 14:33:08 +0300

    Animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Navbar from './components/navbar/Navbar'
 import Main from './pages/Main'
 import Footer from './components/footer/Footer'
 
+import { ReactComponent as BC1 } from './icons/BC1.svg'
+import { ReactComponent as BC2 } from './icons/BC2.svg'
+
 //import bc from './images/BC.png'
 
 const App = () => {
@@ -18,6 +21,8 @@ const App = () => {
      return (
           <>
                {/* <img src={bc} className={press ? 'BC' : 'BCNONE'} alt='Error' onClick={handlerClick} /> */}
+               <BC1 className='BC1' />
+               <BC2 className='BC2' />
                <Navbar />
                <Main />
                <Footer />
